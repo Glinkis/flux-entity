8cbf90beaa4bd7d89422500fa673485ab6aeed47
Victor Glindås <glinkis@gmail.com>
2019-08-20 21:45:38 +0200

    Add insert/remove

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
-export interface Entity<T> {
-  readonly all: Record<string, T>
-  readonly ids: Array<string>
+import { arrayInsert, arrayRemove } from './array'
+
+type Key<T> = { [K in keyof T]: T[K] extends string ? K : never }[keyof T]
+
+export interface Entity<T, K extends Key<T>> {
+  readonly all: Record<any, T>
+  readonly ids: T[K][]
+  readonly key: K
 }
 
-export function createEntity<T>(): Entity<T> {
-  return {
-    all: {},
-    ids: []
+export function createEntityFactory<T>() {
+  return function createEntity<K extends Key<T>>(key: K): Entity<T, K> {
+    return { all: {}, ids: [], key: key }
+  }
+}
+
+const entity = createEntityFactory<Window>()('status')
+insert(entity, new Window())
+
+export function insert<T, K extends Key<T>>(entity: Entity<T, K>, ...values: T[]) {
+  for (const value of values) {
+    const id = value[entity.key]
+    entity.all[id] = value
+    arrayInsert(entity.ids, id)
+  }
+}
+
+export function remove<T, K extends Key<T>>(entity: Entity<T, K>, ...values: T[]) {
+  for (const value of values) {
+    const id = value[entity.key]
+    delete entity.all[id]
+    arrayRemove(entity.ids, id)
   }
 }
