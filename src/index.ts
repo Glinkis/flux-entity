ffafb120f99ed79f9fc5bb4d7b982a73f3e09db1
Victor Glindås <glinkis@gmail.com>
2019-08-20 21:47:53 +0200

    Update types

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,16 @@ import { arrayInsert, arrayRemove } from './array'
 type Key<T> = { [K in keyof T]: T[K] extends string ? K : never }[keyof T]
 
 export interface Entity<T, K extends Key<T>> {
-  readonly all: Record<any, T>
+  readonly all: Record<T[K], T>
   readonly ids: T[K][]
   readonly key: K
 }
 
 export function createEntityFactory<T>() {
   return function createEntity<K extends Key<T>>(key: K): Entity<T, K> {
-    return { all: {}, ids: [], key: key }
+    return { all: {} as any, ids: [], key: key }
   }
 }
-
-const entity = createEntityFactory<Window>()('status')
-insert(entity, new Window())
 
 export function insert<T, K extends Key<T>>(entity: Entity<T, K>, ...values: T[]) {
   for (const value of values) {
