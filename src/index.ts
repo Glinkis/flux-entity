4b20500a6c8d363bcac9c0a84b84277674c40d27
Victor Glindås <glinkis@gmail.com>
2019-08-20 18:26:52 +0200

    Emit esnext

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,10 @@ export interface Entity<T> {
   readonly all: Record<string, T>
   readonly ids: Array<string>
 }
+
+export function createEntity<T>(): Entity<T> {
+  return {
+    all: {},
+    ids: []
+  }
+}
