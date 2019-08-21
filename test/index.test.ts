b4ba656d61926bffd8a59debc78a9eb036aa9788
Victor Glindås <glinkis@gmail.com>
2019-08-21 21:16:48 +0200

    Add unit test for insertValue

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,18 +1,35 @@
 import { expect } from 'chai'
-import { createEntityFactory } from '../src'
+import { createEntityFactory, insertValue } from '../src'
+
+interface Value {
+  id: number
+  name: string
+}
 
 describe('createEntityFactory', () => {
   it('should create a function', () => {
-    expect(createEntityFactory()).to.be.a('function')
+    expect(createEntityFactory<Value>()).to.be.a('function')
   })
 })
 
 describe('createEntity', () => {
   it('should create an entity object', () => {
-    expect(createEntityFactory()('id')).to.eql({
+    expect(createEntityFactory<Value>()('id')).to.eql({
       all: {},
       ids: [],
       key: 'id',
     })
+  })
+})
+
+describe('insertValue', () => {
+  it('should insert a value into the entity', () => {
+    const entity = createEntityFactory<Value>()('id')
+    const value = { id: 0 }
+
+    insertValue(entity, value)
+
+    expect(entity.ids).to.eql([0])
+    expect(entity.all).to.eql({ 0: value })
   })
 })
