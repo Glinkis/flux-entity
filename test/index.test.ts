a3bcab68be68ca54a4bc71a44b038250ab994fef
Victor Glindås <glinkis@gmail.com>
2019-08-21 21:06:33 +0200

    Refactor tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,21 +2,17 @@ import { expect } from 'chai'
 import { createEntityFactory } from '../src'
 
 describe('createEntityFactory', () => {
-  const entityFactory = createEntityFactory()
-
   it('should create a function', () => {
-    expect(entityFactory).to.be.a('function')
+    expect(createEntityFactory()).to.be.a('function')
   })
+})
 
-  describe('the returned function', () => {
-    const entity = entityFactory('id')
-
-    it('should create an entity object', () => {
-      expect(entity).to.eql({
-        all: {},
-        ids: [],
-        key: 'id',
-      })
+describe('createEntity', () => {
+  it('should create an entity object', () => {
+    expect(createEntityFactory()('id')).to.eql({
+      all: {},
+      ids: [],
+      key: 'id',
     })
   })
 })
